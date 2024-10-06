3c504e5711f3035240a0bf26b89e19b61b867cb9
rudyCodesGreatly <rudy.arora.uni@gmail.com>
2024-10-06 12:54:46 -0500

    deployed to firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpz1ktavfDA4v9CvHHC_E4G30KD9yG-lQ",
+  authDomain: "courseschedulertutorial.firebaseapp.com",
+  projectId: "courseschedulertutorial",
+  storageBucket: "courseschedulertutorial.appspot.com",
+  messagingSenderId: "41391965502",
+  appId: "1:41391965502:web:af487d6b4a41682b3b186f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
