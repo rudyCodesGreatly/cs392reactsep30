0db51f30f352ac023333ca7c4bb902234649aafa
rudyCodesGreatly <rudy.arora.uni@gmail.com>
2024-10-08 12:22:13 -0500

    improved ui

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,10 @@ import React from "react";
 
 
 const Banner = ({title}) => (
-    <h1>{title}</h1>
+    <div className="flex justify-center">
+        <h1 className="bg-blue-200 p-5 mb-5 text-center font-semibold text-xl shadow-md inline-block">{title}</h1>
+    </div>
+    
 )
 
 export default Banner;
